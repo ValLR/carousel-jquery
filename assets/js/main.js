50ea7a8b4047493d0fc943599f3f330b7769a4fb
ValLR <valentinalopezrecabal@gmail.com>
2017-07-06 23:55:30 -0400

    avances en función, agregando z-indexes para definir donde quedan las imagenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 	$("#previous").on("click", function(){
 		var currentIm = $(".image-shown");
 		var nextIm = currentIm.next();
-
-		currentIm.
-
+		/*removemos clase shown y la reemplazamos por clase hidden, para cambiar la imagen que se muestra o saca*/
+		currentIm.removeClass("image-shown").addClass("image-hidden").css("z-index",-10);//así la primera será la última en la lista de imágenes
+		nextIm.addClass("image-shown").removeClass("image-hidden").css("z-index", 20)//este quedará al principio de la pila de imágenes
 	});
 });
